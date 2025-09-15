3478140e70137bd2ab0afad8935e840a42466d5c
Albin Sjöstedt <albinsjostedt01@gmail.com>
2025-09-15 11:30:21 +0200

    Reads atomic structure from poscar file

--- pre_processing.py
+++ pre_processing.py
@@ -1,12 +1,22 @@
-import sys
 import json
+from ase.io.vasp import read_vasp
+from ase.visualize import view
 
-class Pre_Processing:
-    def __init__(self, input_settings):
-        self.read_settings(input_settings)
+class PreProcessing:
+    def __init__(self, input_settings, input_structure):
+        self.readSettings(input_settings)
+        self.readAtomicStructure(input_structure)
 
 
-    def read_settings(self, input_settings):
+    def readSettings(self, input_settings):
         with open(input_settings, "r") as file:
             self.settings = json.load(file)
-    
+
+    def readAtomicStructure(self, input_structure):
+        self.atoms = read_vasp(input_structure)
+        view(self.atoms)
+
+
+if __name__ == "__main__":
+    PreProcessing("settings.json", "poscar")
+
