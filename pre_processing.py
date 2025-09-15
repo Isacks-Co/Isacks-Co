fa0be421f37c53b0e3e0796032766cd242e2b78d
Isac Ljungwald <isalj085@student.liu.se>
2025-09-15 11:49:53 +0200

    Print input

--- pre_processing.py
+++ pre_processing.py
@@ -6,6 +6,7 @@ class PreProcessing:
     def __init__(self, input_settings, input_structure):
         self.readSettings(input_settings)
         self.readAtomicStructure(input_structure)
+        self.printInput()
 
 
     def readSettings(self, input_settings):
@@ -15,8 +16,15 @@ class PreProcessing:
     def readAtomicStructure(self, input_structure):
         self.atoms = read_vasp(input_structure)
         view(self.atoms)
+    
+    
+    def printInput(self):
+        for key,value in self.settings.items():
+            print(f"{key} : {value}")
 
+    def readTerminalInput(self):
+        pass
 
 if __name__ == "__main__":
     PreProcessing("settings.json", "poscar")
-
+    
