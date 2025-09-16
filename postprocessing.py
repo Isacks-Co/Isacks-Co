534ea4d855e0e6290716265c964641964db62e2e
Hans-Ove Åkesson <hanak526@student.liu.se>
2025-09-16 16:31:16 +0200

    Basic postprocessing.py that initiates the postprocessing class object and can be vizualize

---
+++ postprocessing.py
@@ -0,0 +1,15 @@
+from ase.io.trajectory import Trajectory
+from ase.visualize import view
+
+
+class PostProcessing:
+    """
+        Uses the log files generates from the Molecular Dynamics class to vizualize and analyze the data
+        input trajectory file as traj_file 
+    """
+    def __init__(self, traj_file):
+        self.read_traj_file = traj_file
+        
+    def vizualize(self):
+        view(Trajectory(self.read_traj_file))
+    
