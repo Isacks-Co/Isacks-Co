f9c2ab962ddcd4d49279c1e8c4aa7abccc3b1780
Albin Sjöstedt <albinsjostedt01@gmail.com>
2025-09-25 10:13:29 +0200

    Runs on pytest now

--- Tests/TestPreProcessing.py
+++ Tests/TestPreProcessing.py
@@ -8,14 +8,14 @@ from ase.visualize import view
 class TestPreProcessing(TestBase):
 
     def testSettings(self):
-        PP = PreProcessing("Tests/testsettings.json", "Tests/TESTPOSCAR", None)
+        PP = PreProcessing("testsettings.json", "TESTPOSCAR", None)
         PP.sanityCheckSettings()
 
     def testAtomicStructure(self):
-        PP = PreProcessing("Tests/testsettings.json", "Tests/TESTPOSCAR", None)
+        PP = PreProcessing("testsettings.json", "TESTPOSCAR", None)
         assert len(PP.atoms) == 125
 
     def testCreateMD(self):
-        PP = PreProcessing("Tests/testsettings.json", "Tests/TESTPOSCAR", None)
+        PP = PreProcessing("testsettings.json", "TESTPOSCAR", None)
         MD = PP.createMD()
         assert str(MD.integrator).startswith("functools.partial(<function VelocityVerlet")
