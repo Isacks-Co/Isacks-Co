Rebased the with refactor main and fixed computation of debye temp

SourceCode/MDBase.py
@@ -149,8 +149,8 @@ class MDBase: #TODO Look at unit conversions
         dyn.attach(lambda: self.save_data(atoms,traj),
                    interval=self.interval)
 
-        dyn.attach(lambda: self.checkDivergence(atoms),
-                   interval=self.interval)
+        #dyn.attach(lambda: self._checkDivergence(atoms),
+                 #  interval=self.interval)
 
         # Continue with the main MD run
         dyn.run(self.steps)  # RUN
@@ -257,7 +257,7 @@ class MDBase: #TODO Look at unit conversions
         if not np.isfinite(current_T):
             raise RuntimeError(f"Divergence appeared, temperature is NaN/Inf")
 
-        if (current_T > div_factor * self.temperature_k):
+        if (current_T > div_factor * self.temperature_k and self.temperature_k != 0):
             raise RuntimeError(f"Divergence: T={current_T:.1f} K >  {div_factor * self.temperature_k:.1f} K, "
                                f"(div_factor * desired temp).")
 

SourceCode/quantityCalculator.py
@@ -317,7 +317,7 @@ class QuantityCalculator:
 
             logger.info(f"N = {N} : V_A3 = {V_A3}")
 
-            Theta_D = (hbar / kB) * (6.0 * np.pi ** 2 * n) ** (1.0 / 3.0) * sound_velocity
+            Theta_D = (hbar / kB) * ((6.0 * np.pi ** 2 * n) ** (1.0 / 3.0)) * sound_velocity / 10.18 # NEED TO DO SQRT(ev/u) to fs/Å
             logger.info(f"Debye temperature: {Theta_D} K")
             return Theta_D
 