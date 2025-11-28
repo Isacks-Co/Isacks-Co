084bf2ed6ecd44df9c4c2bd1edbea124a8386536
isaha538 <isaha538@student.liu.se>
2025-11-28 16:57:46 +0100

    First attempt at merging dev with task 96. Before merging with dev, a group member who is more familiar with the code structure and how to run the code on dev, needs to take a look at my merge.

--- SourceCode/MDManager.py
+++ SourceCode/MDManager.py
@@ -1,6 +1,6 @@
 import logging
 import copy
-from MDBase import MDBase
+from MDClasses import EquilibriumRun, SampleRun, StrecthRun
 
 logger = logging.getLogger(__name__)
 
@@ -19,35 +19,45 @@ class MDManager:
         """
         self.sim_list = sim_list
         self.quants = quants
-        q_sims_nve_deps = {"equil": ["cve"], "sample": ["cve"], "stretch1": [], "stretch2": [], "stretch3": [], "any": [] }
-        q_sims_nvt_deps = {"equil": ["p_i", "b1", "b2", "b3", "g2", "g3", "cvt", "debye"], "sample": ["cvt"], "stretch1": ["p_i", "b1"], "stretch2": ["b2", "g2", "debye"], "stretch3": ["b3", "g3", "debye"], "any": []}
-        q_sims_npt_deps ={"equil": [], "sample": ["lat_const"], "stretch1": [], "stretch2": [], "stretch3": [], "any": []}
-        q_sims_indep_of_ensemble = {"equil": ["d", "e_coh", "l_crit", "lat_const"], "sample": ["e_coh"], "stretch1": [], "stretch2": [], "stretch3": [] , "any": ["msd", "l_crit"]}
-        self.catergorized_compatibility = [q_sims_nve_deps, q_sims_nvt_deps, q_sims_npt_deps, q_sims_indep_of_ensemble]
+        q_sims_nvt_deps = {"equil": ["p_i", "b", "g", "cvt", "debye"], "sample": ["cvt"], "stretch": ["p_i", "b"], "any": []}
+        q_sims_npt_deps ={"equil": [], "sample": ["lat_const"], "stretch": [], "any": []}
+        q_sims_indep_of_ensemble = {"equil": ["d", "e_coh", "l_crit", "lat_const"], "sample": ["e_coh"], "stretch": [] , "any": ["msd", "l_crit"]}
+        self.catergorized_compatibility = [q_sims_nvt_deps, q_sims_npt_deps, q_sims_indep_of_ensemble]
         self.order_of_operations = _simulations_to_run(sim_list, self.catergorized_compatibility, quants)
 
 
-    def run(self, atoms):
+    def run(self, atomic_structure):
         for sim in self.order_of_operations:
-            logger.info(f"Running {sim}")
+            logger.debug(f"Running {sim}")
             for ensemble, variations in sim.items():
-                match ensemble:
-                    case "npt":
-                        for var in variations:
-                            MD = MDBase(self.sim_list[0], ensemble, var)
-                            MD.simulate(atoms)
-                    case "nve":
-                        for var in variations:
-                            MD = MDBase(self.sim_list[1], ensemble, var)
-                            MD.simulate(atoms)
-                    case "nvt":
-                        for var in variations:
-                            MD = MDBase(self.sim_list[2], ensemble, var)
-                            MD.simulate(atoms)
-                    case "indep":
-                        for var in variations:
-                            MD = MDBase(self.sim_list[2], ensemble, var)
-                            MD.simulate(atoms)
+                logger.debug(f"Variations: {variations}")
+                for var in variations:
+                    self.simulate(ensemble=ensemble, variation=var, atomic_structure=atomic_structure)
+
+    def simulate(self, ensemble, variation, atomic_structure):
+        if variation == "equil":
+            equil_settings = self.sim_list[0]
+            equil_MD = EquilibriumRun(settings=equil_settings)
+            logger.info("Relaxing structure")
+            equil_struct = equil_MD.run(atomic_structure, equil_settings.num_steps, init_vel=True)
+
+        if variation == "sample" or variation == "any":
+            if ensemble == "nvt":
+                sample_settings = self.sim_list[1]
+            else:
+                sample_settings = self.sim_list[2]
+            sample_MD = SampleRun(settings=self.sim_list[1])
+            logger.info("Sampling structure")
+            sample_data = sample_MD.run(equil_struct, sample_settings.num_steps)
+
+        if variation == "stretch":
+            stretch_MD = StrecthRun(settings=self.sim_list[1])
+            logger.info("Running stretch sequence")
+            sample_data.storeTxtFile()
+            C_matrix = stretch_MD.run(equil_struct)
+
+        logger.info("MD done")
+        logger.info(f"Stored results in {equil_struct.label}/Outputfiles")
 
 
 def _simulations_to_run(sim_list, cat_resp, quants = None):
@@ -71,15 +81,9 @@ def _simulations_to_run(sim_list, cat_resp, quants = None):
                 if q in deps["sample"]:
                     if "sample" not in candidate_run_variations:
                         candidate_run_variations.append("sample")
-                if q in deps["stretch1"] :
-                    if "stretch1" not in candidate_run_variations:
-                        candidate_run_variations.append("stretch1")
-                if q in deps["stretch2"] :
-                    if "stretch2" not in candidate_run_variations:
-                        candidate_run_variations.append("stretch2")
-                if q in deps["stretch3"] :
-                    if "stretch3" not in candidate_run_variations:
-                        candidate_run_variations.append("stretch3")
+                if q in deps["stretch"] :
+                    if "stretch" not in candidate_run_variations:
+                        candidate_run_variations.append("stretch")
                 """
                 if q in deps["any"] :
                     if "any" not in candidate_run_variations:
@@ -88,12 +92,10 @@ def _simulations_to_run(sim_list, cat_resp, quants = None):
 
             if candidate_run_variations:
                 if categorized_compatibility.index(deps) == 0:
-                    order_of_operations.append({"nve": candidate_run_variations})
-                elif categorized_compatibility.index(deps) == 1:
                     order_of_operations.append({"nvt": candidate_run_variations})
-                elif categorized_compatibility.index(deps) == 2:
+                elif categorized_compatibility.index(deps) == 1:
                     order_of_operations.append({"npt": candidate_run_variations})
-                elif categorized_compatibility.index(deps) == 3:
+                elif categorized_compatibility.index(deps) == 2:
                     order_of_operations.append({"indep": candidate_run_variations})
             candidate_run_variations = []
 
