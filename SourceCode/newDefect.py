91ee764a4692c1c304131cf558ca63a1a044c426
NRSkog <skogemyr.ruben@gmail.com>
2025-11-30 13:24:46 +0100

    removed uneccesary lines in the classes and added comments to make it more clear

--- SourceCode/newDefect.py
+++ SourceCode/newDefect.py
@@ -35,6 +35,7 @@ from httk.core.vectors import FracVector
 class newDefectInfos(httk.HttkObject):
     #Class to primary search for info on the specific defect, contains info on host- and defect names, types etc
     #Might not be as useful to us, since we probably only need newDefectCell
+    #Could be fun to implement in the pipeline either way
 
     @httk.httk_typed_init({'key': int, 'host_name': str, 'defect_name': str, 'defect_type': str, 'configuration': str, 'vacancy': bool,
                            'substitutional': bool, 'interstitial': bool}, index=['key', 'defect_name',  'defect_type', 'configuration',
@@ -54,48 +55,43 @@ class newDefectInfos(httk.HttkObject):
         self.substitutional = substitutional
         self.interstitial = interstitial
 
+
 class newDefectCell(httk.HttkObject):
-    #Contains the structural information on the defect
-    @httk.httk_typed_init({'host_name': str, 'defect_structure': Structure, 'defect_name': str, 'defect_types': [str],
-                           'defect_positions': (FracVector, 0, 3), 'key': int },
+    #Contains the relaxed structure and related info
+    @httk.httk_typed_init({'host_name': str, 'defect_structure': Structure, 'defect_name': str,
+                            'key': int },
                           index=['defect_name', 'key'])
 
-    def __init__(self, host_name, defect_structure, defect_name, defect_types, defect_positions, key):
+    def __init__(self, host_name, defect_structure, defect_name, key):
         self.host_name = host_name
         self.defect_structure = defect_structure
-        # unique name for the defect
         self.defect_name = defect_name
-        # what defect is it
-        self.defect_types = defect_types
-        # and where in the supercell it is localated
-        self.defect_positions = defect_positions
-        # unique hash for the defect
         self.key = key
-        # priority for the defect
-        # general description of the defect
+
 
 class newScreenResult(httk.Result):
+    #Stores relaxation energy and info
+    @httk.httk_typed_init({
+        'defect_key': int,
+        'computation': httk.Computation,
+        'defect_folder_name': str,
+        'total_energy_coarse': float,
+        'max_relaxation': float,
+        'average_relaxation': float
+    }, index=['defect_key'])
+    def __init__(self, defect_key, computation, defect_folder_name, total_energy_coarse, max_relaxation, average_relaxation):
 
-    @httk.httk_typed_init({'defect_key': int, 'computation': httk.Computation,   'defect_folder_name': str, 'total_energy_coarse': float, 'smallest_abs_estimate': float, 'smallest_abs_transition_intensity': float, 'smallest_abs_partial_density_difference': float, 'smallest_abs_polarization_x': float, 'smallest_abs_polarization_y': float, 'smallest_abs_polarization_z': float, 'excitation': str, 'abs_estimate': float, 'abs_transition_intensity': float, 'abs_partial_density_difference': float, 'abs_polarization_x': float, 'abs_polarization_y': float, 'abs_polarization_z': float, 'abs_radiative_lifetime': float, 'ZPL_converged': bool, 'ZPL_estimate': float, 'ZPL_transition_intensity': float, 'ZPL_partial_density_difference': float, 'ZPL_polarization_x': float, 'ZPL_polarization_y': float, 'ZPL_polarization_z': float, 'ZPL_radiative_lifetime': float, 'max_relaxation': float, 'average_relaxation': float, 'delta_R': float, 'delta_Q': float, 'huang_rhys': float, 'debye_waller': float, 'omega': float, 'time': float}, index=['defect_key'])
-    def __init__(self, defect_key, computation, defect_folder_name, total_energy_coarse, smallest_abs_estimate, smallest_abs_transition_intensity, smallest_abs_partial_density_difference, smallest_abs_polarization_x, smallest_abs_polarization_y, smallest_abs_polarization_z, excitation, abs_estimate, abs_transition_intensity, abs_partial_density_difference, abs_polarization_x, abs_polarization_y, abs_polarization_z, abs_radiative_lifetime, ZPL_converged, ZPL_estimate, ZPL_transition_intensity, ZPL_partial_density_difference, ZPL_polarization_x, ZPL_polarization_y, ZPL_polarization_z, ZPL_radiative_lifetime, max_relaxation, average_relaxation, delta_R, delta_Q, huang_rhys, debye_waller, omega, time):
         self.defect_key = defect_key
         self.computation = computation
         self.defect_folder_name = defect_folder_name
         self.total_energy_coarse = total_energy_coarse
-
-        # relaxation from ground to excited geometry
         self.max_relaxation = max_relaxation
         self.average_relaxation = average_relaxation
 
 
 
-
-
-
 class newDelta(httk.HttkObject):
-    """
-
-    """
+    #Stores delta value, most stable defect
     @httk.httk_typed_init({'host': str, 'dopant': str, 'defect': str, 'key': int, 'delta': float})
     def __init__(self, host, dopant, defect, key, delta):
         self.host = host
--- SourceCode/newDefectUtils.py
+++ SourceCode/newDefectUtils.py
@@ -5,38 +5,41 @@ from newDefect import newScreenResult
 
 
 def getAllDefectKeys(store):
-
     search = store.searcher()
     search_defectcell = search.variable(DefectCell)
 
-    search.output(search_defectcell.host_name, "host")
-    search.output(search_defectcell.defect_name, "defect")
+    #smarter to use keys
+    #search.output(search_defectcell.host_name, "host")
+    #search.output(search_defectcell.defect_name, "defect")
+    search.output(search_defectcell.key, "defect_key")
 
     all_keys = set()
-
     for match, header in search:
-        host_name = match[0]
-        defect_name = match[1]
+        #host_name = match[0]
+        #defect_name = match[1]
+        key = match[0]
 
-        all_keys.add((host_name, defect_name))
+        #all_keys.add((host_name, defect_name))
+        all_keys.add(key)
 
     return all_keys
 
 
 def getMDKeys(store):
     search = store.searcher()
-    search_defectcell = search.variable(newScreenResult)
+    search_MDcell = search.variable(newScreenResult)
 
-    search.output(search_defectcell.host_name, "host")
-    search.output(search_defectcell.defect_name, "defect")
+    #search.output(search_defectcell.host_name, "host")
+    #search.output(search_defectcell.defect_name, "defect")
+    search.output(search_MDcell.defect_key, "defect_key")
 
     all_md_keys = set()
-
     for match, header in search:
-        host_name = match[0]
-        defect_name = match[1]
+        #host_name = match[0]
+        #defect_name = match[1]
+        key = match[0]
 
-        all_md_keys.add((host_name, defect_name))
+        all_md_keys.add(key)
 
     return all_md_keys
 
@@ -44,13 +47,11 @@ def allDoneMDRuns(store):
     defect_keys = getAllDefectKeys(store)
     md_keys = getMDKeys(store)
 
-    missing = defect_keys - md_keys #ger tillbaka lista/set med de nycklar som saknas
-    if missing:
-        print("MISSING MD FOR: ")
-        for host, defect in sorted(missing):
-            print(host, defect)
+    missing_keys = defect_keys - md_keys #ger tillbaka lista/set med de nycklar som saknas
+    if missing_keys:
+        print(f"These keys are missing: {sorted(missing_keys)} ")
         return False
-    
+
     return True
 
 
