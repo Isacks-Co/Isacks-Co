6cf9e03abac7b8706aaef9da24b19750bee79034
Albin Sjöstedt <albinsjostedt01@gmail.com>
2025-12-04 11:31:32 +0100

    Working versions of getResults and ht_steps

--- SourceCode/MDClasses.py
+++ SourceCode/MDClasses.py
@@ -118,8 +118,8 @@ class EquilibriumRun(MDBase):
     
     def _check_equilibrium(self):
         if len(self.equil_data) > 100:
-            if EquilibriumCondition.checkStable(self.equil_data[-100:], 0.05):
-                
+            if EquilibriumCondition.checkStable(self.equil_data[-100:], 0.01):
+                print(f"Stopped with equilibrium after {len(self.equil_data)}")
                 raise StopIteration(f"Equil reached")
     def _saveData(self, atomic_structure):
         self.equil_data.append(atomic_structure.potential_energy)
