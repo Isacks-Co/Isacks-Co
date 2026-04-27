411cf6bd6d19a019749a6f8a4ad3773f6bb49c92
Albin Sjöstedt <x_albsj@sigma.nsc.liu.se>
2026-04-27 21:29:40 +0200

    A bit harsher criterium, shows good results

--- SourceCode/MDClasses.py
+++ SourceCode/MDClasses.py
@@ -256,7 +256,7 @@ class EquilibriumRun(MDBase):
         if self.integrator.ensemble == "NVT":
             if len(self.equil_data) > 1000:
                 atomic_structure.final_energy_mean = np.mean(self.equil_data[-500:])
-                if EquilibriumCondition.checkStable(self.equil_data[-1000:],len(atomic_structure) , 1e-3):
+                if EquilibriumCondition.checkStable(self.equil_data[-1000:],len(atomic_structure) , 1e-4):
                     self.flag = 0
                     print(f"Stopped with equilibrium after {len(self.equil_data)}")
                     raise StopIteration(f"Equil reached")
